facf658765e69a48a66f85d1339c61dc92bd327a
Patrik Weibull <patrik.weibull@reguity.com>
2021-05-13 15:01:28 +0000

    Optimization for large amount of seats/shares

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 module.exports = ({ seats, votes, startDivisorFn, divisorFn }) => {
+  let seatsLeft = seats;
+  const numParties = Object.keys(votes).length;
+  const totalNumVotes = Object.values(votes).reduce((ack, v) => ack + v, 0);
   let byParty = Object.entries(votes)
-    .reduce((ack, [ party, numVotes ]) => ({
-      ...ack,
-      [party]: { seats: 0, votes: numVotes, divisor: startDivisorFn(numVotes) }
-    }), {});
-  while (seats > 0) {
+    .reduce((ack, [ party, numVotes ]) => {
+      const allocatedSeats = Math.floor(seats * numVotes / totalNumVotes);
+      seatsLeft -= allocatedSeats;
+      return {
+        ...ack,
+        [party]: {
+          seats: allocatedSeats,
+          votes: numVotes,
+          divisor: allocatedSeats === 1 ? startDivisorFn(numVotes) : divisorFn(numVotes, allocatedSeats)
+        }
+      }
+    }, {});
+  while (seatsLeft > 0) {
     let party = Object.entries(byParty).reduce((a, [ k, v ]) => a.divisor > v.divisor ? a : v, { divisor: 0 });
     party.seats++;
     party.divisor = divisorFn(party.votes, party.seats);
-    seats--;
+    seatsLeft--;
   }
   return Object.entries(byParty).reduce((a, [k, v]) => ({ ...a, [k]: v.seats }), {});
 };
